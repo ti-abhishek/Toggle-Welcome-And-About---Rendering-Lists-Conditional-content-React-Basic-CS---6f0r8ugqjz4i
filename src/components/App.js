286c8f98118ff6c87ab6384223836871ef3ac5a5
ti-abhishek <ti.abhishek15@gmail.com>
2022-12-12 23:26:15 +0530

    n

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
-  const [welcome, setWelcome] = React.useState(true);
-
+  const [state, setState] = useState(true)
+  const handleToggle = () => {
+       if(state === true) {
+            setState(false)
+       } else {
+        setState(true)
+       }
+  }
   return (
     <div id="main">
-      {welcome ? (
-        <div id="welcome-div">
-          <h1>Welcome to Newton School!!</h1>
-        </div>
-      ) : (
-        <div id="about-div">
-          <h1>
-            Newton School is a neo-university providing highly immersive tech
-            learning to millions of students, to tap into new-age tech
-            opportunities.
-          </h1>
-        </div>
-      )}
-      <button id="toggle" onClick={() => setWelcome(!welcome)}>
-        Toggle
-      </button>
+      {state === true ? (
+        <div id="welcome-div">{<h1>Welcome to Newton School!!</h1>}</div>) : (
+        <div id="about-div">{<h1>Newton School is a neo-university providing highly
+           immersive tech learning to millions of students, to tap into new-age 
+           tech opportunities.</h1>}</div>
+        )}
+      <button id="toggle" onClick={handleToggle}>Toggle</button>
     </div>
   );
 };
