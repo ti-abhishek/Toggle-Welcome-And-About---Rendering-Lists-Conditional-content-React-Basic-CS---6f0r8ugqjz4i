355b9bf00aed819fb1d66886761978158fb2684c
ti-abhishek <ti.abhishek15@gmail.com>
2022-12-12 23:16:28 +0530

    n

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,26 @@ import React from "react";
 import "../styles/App.css";
 
 const App = () => {
+  const [welcome, setWelcome] = React.useState(true);
+
   return (
     <div id="main">
-      <div id="welcome-div">
-        <h1>Welcome to Newton School!!</h1>
-      </div>
-      <div id="about-div">
-        <h1>
-          Newton School is a neo-university providing highly immersive tech
-          learning to millions of students, to tap into new-age tech
-          opportunities.
-        </h1>
-      </div>
-      <button id="toggle">Toggle</button>
+      {welcome ? (
+        <div id="welcome-div">
+          <h1>Welcome to Newton School!!</h1>
+        </div>
+      ) : (
+        <div id="about-div">
+          <h1>
+            Newton School is a neo-university providing highly immersive tech
+            learning to millions of students, to tap into new-age tech
+            opportunities.
+          </h1>
+        </div>
+      )}
+      <button id="toggle" onClick={() => setWelcome(!welcome)}>
+        Toggle
+      </button>
     </div>
   );
 };
